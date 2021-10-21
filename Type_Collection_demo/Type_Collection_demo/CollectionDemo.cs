3b512a12c893eec45d7a7b48d7f8d5e4a5f586b9
Aditi-99326 <89843862+Aditi-99326@users.noreply.github.com>
2021-10-21 23:05:17 +0530

    Add files via upload

---
+++ Type_Collection_demo/Type_Collection_demo/CollectionDemo.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace Type_Collection_demo
+{
+    class CollectionDemo
+    {
+        static void Main(string[] args)
+        {
+            #region Array
+            ////datatype[] name = new datatype[size];
+
+            ////declare 
+            //int[] numberArray = new int[3];
+
+            ////initialize
+            //numberArray[0] = 454;
+            //numberArray[1] = 44754;
+            //numberArray[2] = 23;
+
+            ////declare and initialize
+            //int[] numberArray = {45, 34,6758,5522};
+
+
+            //for (int i = 0; i < numberArray.Length; i++)
+            //{
+            //    Console.WriteLine(numberArray[i]);
+            //}
+            #endregion
+
+            #region System.Collections classes
+            /* ArrayList countryList = new ArrayList();
+             countryList.Add("India");            //0
+             countryList.Add("USA");              //2
+             countryList.Insert(1 ,"France");     //1
+
+             countryList.Add(100); // store int value - 3
+             countryList.Add(true); // store bool value - 4
+
+             //object initializer { }
+             Person person = new Person() { FirstName = "Aditi", LastName = "Godse"};
+
+             Console.WriteLine(person.FirstName + " " + person.LastName);
+
+             Console.WriteLine("-----------------------------------------\n");
+
+             countryList.Add(person);   //5
+
+             //from countryList collection for item no 5 (person object) print only FirstName
+
+             //Type conversion
+
+             Console.WriteLine(((Person)countryList[5]).FirstName );
+
+
+             Console.WriteLine("-----------------------------------------\n");
+
+             for (int i = 0; i < countryList.Count; i++)
+             {
+                 Console.WriteLine(countryList[i]);
+             } */
+
+            #endregion
+
+            #region Generic Collection classes
+
+            /* List<string> countryList = new List<string>();
+             countryList.Add("India");
+             //countryList.Add(100);
+
+             List<int> numbers = new List<int>();
+             numbers.Add(100);
+
+             List<Person> personList = new List<Person>();
+             personList.Add(new Person() { FirstName = "aditi", LastName = "godse" });
+
+             Console.WriteLine(personList[0].FirstName); */
+
+            #endregion
+
+            #region Nullable Type
+
+            //int i = null;
+
+            Nullable<int> x = null;
+            x = 100;
+            x = null;
+
+            //datatype?   ----------nullable type
+            int? y = null;
+
+            #endregion
+
+        }
+    }
+}
