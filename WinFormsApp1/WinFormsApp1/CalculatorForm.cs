5bf575d08de80b03c42de27c2daf9a0a842fc963
Aditi-99326 <89843862+Aditi-99326@users.noreply.github.com>
2021-10-21 23:09:48 +0530

    Add files via upload

---
+++ WinFormsApp1/WinFormsApp1/CalculatorForm.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormsApp1
+{
+    public partial class CalculatorForm : Form
+    {
+        double operand1, operand2, Result;
+        public CalculatorForm()
+        {
+            InitializeComponent();
+        }
+
+        private void lblFirst_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtFirst_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            operand1 = Convert.ToDouble(txtFirst.Text);
+            operand2 = Convert.ToDouble(txtSecond.Text);
+            Result = operand1 + operand2;
+            txtResult.Text = Result.ToString();
+        }
+
+        private void btnMinus_Click(object sender, EventArgs e)
+        {
+            operand1 = Convert.ToDouble(txtFirst.Text);
+            operand2 = Convert.ToDouble(txtSecond.Text);
+            Result = operand1 - operand2;
+            txtResult.Text = Result.ToString();
+
+        }
+
+        private void btnMultiplication_Click(object sender, EventArgs e)
+        {
+            operand1 = Convert.ToDouble(txtFirst.Text);
+            operand2 = Convert.ToDouble(txtSecond.Text);
+            Result = operand1 * operand2;
+            txtResult.Text = Result.ToString();
+        }
+
+        private void btnDivision_Click(object sender, EventArgs e)
+        {
+            if (Convert.ToDouble(txtSecond.Text) != 0)
+            {
+                operand1 = Convert.ToDouble(txtFirst.Text);
+                operand2 = Convert.ToDouble(txtSecond.Text);
+                Result = operand1 / operand2;
+                txtResult.Text = Result.ToString();
+            }
+
+            else
+            {
+                
+            }
+        }
+
+        private void txtFirst_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if(char.IsDigit(e.KeyChar) == false && char.IsControl(e.KeyChar)  == false && (e.KeyChar == '.'? txtFirst.Text.Contains('.') == true : true))
+            e.Handled = true;
+        }
+
+        private void txtSecond_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (char.IsDigit(e.KeyChar) == false && char.IsControl(e.KeyChar) == false && (e.KeyChar == '.' ? txtSecond.Text.Contains('.') == true : true))
+                e.Handled = true;
+        }
+
+        private void txtResult_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void txtResult_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (char.IsDigit(e.KeyChar) == false && char.IsControl(e.KeyChar) == false && (e.KeyChar == '.' ? txtFirst.Text.Contains('.') == true : true))
+                e.Handled = true;
+        }
+
+        private void CalculatorForm_Load(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void lblSecond_Click(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
---
+++ WinFormsApp1/WinFormsApp1/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void lblPassword_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txt_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblMsg_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSignin_Click(object sender, EventArgs e)
+        {
+            //username - admin and password - 123
+
+            //txtUsername.Text ----- username
+            //txtpwd.Text ----- passaword
+
+            //lblmsg.Text = "valid user";
+            //lblmsg.Text = "Invalid username or passaword";
+
+            if (txtUserName.Text == "admin" && txtPassaword.Text == "123")
+            {
+                lblMsg.Text = "Valid User";
+
+            }
+            else
+            {
+                lblMsg.Text = "Invalid UserName or passaword";
+            }
+        }
+
+        
+    }
+}
