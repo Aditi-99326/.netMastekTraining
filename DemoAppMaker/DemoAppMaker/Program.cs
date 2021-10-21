75fa60cf3f9da7276b0420e07f7423e9752cdf4c
Aditi-99326 <89843862+Aditi-99326@users.noreply.github.com>
2021-10-21 22:31:34 +0530

    Add files via upload

---
+++ DemoAppMaker/DemoAppMaker/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace DemoAppMaker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Welcome to .net");
+        }
+    }
+}
